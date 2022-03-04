26d2367aa1f213fd45641130813c334221bc8ddb
samu2421 <95489339+samu2421@users.noreply.github.com>
2022-03-04 19:08:56 +0530

    Add files via upload

---
+++ printing positive numbers.py
@@ -0,0 +1,10 @@
+print("Enter the no's:")
+n = list(input().split())
+print("Entered list is : ", n)
+
+output_list = []
+
+for i in n:
+    if int(i) > 0:
+        output_list.append(int(i))
+print("Positive numbers from given list are : ", output_list)
